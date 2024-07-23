c0d5ebe6bf886ad54d15cfd73e493d14ae3dcfd1
haykmanasyan <cloudstatic01@gmail.com>
2024-07-23 11:30:54 -0400

    added view.html

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
